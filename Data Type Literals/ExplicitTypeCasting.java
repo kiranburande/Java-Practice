b30a87151899d39539a965f52931491686475559
Kiran Burande <kiransburande6172@gmail.com>
2024-10-09 18:22:06 +0530

    Add files via upload

---
+++ Data Type Literals/ExplicitTypeCasting.java
@@ -0,0 +1,14 @@
+package com.literals;
+
+public class ExplicitTypeCasting 
+{
+	public static void main (String[]args)
+	 {
+	 double doubleVariable = 100.04;
+	 long longVariable = (long) doubleVariable;
+	 int intVariable = (int) longVariable;
+	 System.out.println ("Double Value is : " + doubleVariable);
+	 System.out.println ("Long Value is : " + longVariable);
+	 System.out.println ("Integer Value is : " + intVariable);
+	 }
+}
