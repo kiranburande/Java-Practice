15ce6cc422d6527ca73540da1bbd85fa7f7cb03d
Kiran Burande <kiransburande6172@gmail.com>
2024-10-17 18:39:58 +0530

    Add files via upload

---
+++ Code Practice/Day 1/SumOfFirst100NaturalNumbers.java
@@ -0,0 +1,20 @@
+package PracticeJava;
+
+public class SumOfFirst100NaturalNumbers 
+{
+	public static void main(String[] args) 
+	{
+		int sum=0;
+		
+		for(int x=1;x<=100;x++)
+		{
+			sum=sum+x;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
+ 
+
+/*  WAP in java to print the sum of first hundred natural number */
