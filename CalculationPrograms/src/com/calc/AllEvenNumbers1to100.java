fa622e29d2f770cd4f938d6e58fa7ae23a6e45a0
Kiran Burande <kiransburande6172@gmail.com>
2024-10-27 12:21:58 +0530

    Add files via upload

---
+++ CalculationPrograms/src/com/calc/AllEvenNumbers1to100.java
@@ -0,0 +1,19 @@
+package PracticeJava;
+
+public class AllEvenNumbers1to100 
+{
+	public static void main(String[] args) 
+	{
+	
+	int x=1;
+	
+	while(x<=100)
+	{
+		if(x%2==0)System.out.println(x);
+		x++;
+	}
+	}
+
+}
+  
+/*   WAP in java to print all even numbers from 1 - 100 */
