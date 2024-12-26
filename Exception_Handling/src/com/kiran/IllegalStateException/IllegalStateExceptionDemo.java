5106bf2a6a717a5a11975e3d14ca2f3040e3adc8
Kiran Burande <kiransburande6172@gmail.com>
2024-12-25 23:01:12 -0800

    Add files via upload

---
+++ Exception_Handling/src/com/kiran/IllegalStateException/IllegalStateExceptionDemo.java
@@ -0,0 +1,59 @@
+package com.kiran.IllegalStateException;
+
+public class IllegalStateExceptionDemo 
+{
+	static void throwIllegalException()
+	{
+		try {
+			throw new IllegalStateException("MyException"); 
+		}
+		catch(IllegalStateException objA)
+		{
+			System.out.println("caught:" +objA);
+		}
+	}
+	public static void main(String[] args) 
+	{
+		throwIllegalException();
+
+	}
+
+}
+
+/*   
+Ques - 3
+--------------
+IllegalStateException 
+
+a. Write a program to create an ELC class IllegalStateExceptionDemo which implements a method as follows 
+   static void throwIllegalException( ) { 
+	try{ 
+		throw new IllegalStateException("MyException"); 
+	} catch(IllegalStateException objA){
+		System.out.println("caught:" +objA); 
+	}
+    }
+
+b. Implement the main method, and from main method call the above method.
+
+c. Check the output of the program.
+
+Test Case 1:
+Input:
+No input required, just running the program.
+
+Output:
+Calling throwIllegalException from main method:
+Caught: java.lang.IllegalStateException: MyException
+
+Test Case 2:
+Input:
+No input required, just running the program.
+
+Output:
+Calling throwIllegalException from main method:
+Caught: java.lang.IllegalStateException: MyException
+
+
+ */
+
