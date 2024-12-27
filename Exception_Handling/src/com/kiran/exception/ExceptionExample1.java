a70cd5c53ccf8f94736ef75797f0ab6d2a449310
Kiran Burande <kiransburande6172@gmail.com>
2024-12-26 22:48:02 -0800

    Add files via upload

---
+++ Exception_Handling/src/com/kiran/exception/ExceptionExample1.java
@@ -0,0 +1,14 @@
+package com.kiran.exception;
+
+public class ExceptionExample1 {
+    public static void main(String[] args) {
+        try {
+            int[] nums = {1, 2, 3};
+            System.out.println(nums[3]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException occurred.");
+        } finally {
+            System.out.println("End of program.");
+        }
+    }
+}
