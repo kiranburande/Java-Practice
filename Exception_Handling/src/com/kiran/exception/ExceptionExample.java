814147679386430094fe182f89be551a1b56e0dd
Kiran Burande <kiransburande6172@gmail.com>
2024-12-26 22:47:12 -0800

    Add files via upload

---
+++ Exception_Handling/src/com/kiran/exception/ExceptionExample.java
@@ -0,0 +1,14 @@
+package com.kiran.exception;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException occurred.");
+        } finally {
+            System.out.println("End of program.");
+        }
+    }
+}
