13091e3a8409ba0daf9c326503d8508ed0a63a0d
Kiran Burande <kiransburande6172@gmail.com>
2024-12-26 22:48:56 -0800

    Add files via upload

---
+++ Exception_Handling/src/com/kiran/exception/ExceptionExample3.java
@@ -0,0 +1,16 @@
+package com.kiran.exception;
+
+public class ExceptionExample3 {
+    public static void main(String[] args) {
+        try {
+            int num = Integer.parseInt("abc");
+            System.out.println("The number is: " + num);
+        } 
+        catch (NumberFormatException e) {
+            System.out.println("NumberFormatException occurred.");
+        } 
+        finally {
+            System.out.println("End of program.");
+        }
+    }
+}
